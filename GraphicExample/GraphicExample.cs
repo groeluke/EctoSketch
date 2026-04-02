9a2cab9b7ead545d439ff3186b29ee5143361343
lukeg <groeluke@isu.edu>
2026-04-02 08:13:10 -0600

    Add project files.

---
+++ GraphicExample/GraphicExample.cs
@@ -0,0 +1,10 @@
+namespace GraphicExample
+{
+    public partial class GraphicExample : Form
+    {
+        public GraphicExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
